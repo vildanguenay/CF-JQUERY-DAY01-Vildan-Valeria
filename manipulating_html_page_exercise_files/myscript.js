d8465c22bd3e862700b93d385200acff0bfbdb37
Valeria Nazarova <alira.vn@gmail.com>
2019-08-06 12:01:23 +0200

    create 5 divs on one click

--- manipulating_html_page_exercise_files/myscript.js
+++ manipulating_html_page_exercise_files/myscript.js
@@ -1,8 +1,15 @@
 $("form").submit(function(e) {
 
 	e.preventDefault();
+	// $("form").remove();	
 	
-	
-	
+	// CREATES 5 DIVS ON EACH CLICK
+	for (i = 0; i < 5; i++) {
+		var newDiv = $(`<div>DIV</div>`)
+		$("article").prepend(newDiv)
+	}
+	// CREATES ONE DIV ON EACH CLICK
+	// var newDiv = $(`<div>DIV</div>`);
+	// $("article").prepend(newDiv);
 
 });
