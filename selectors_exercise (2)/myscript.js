bb623e69b3ae99596bcbf544a7de7812667dffaf
Valeria Nazarova <alira.vn@gmail.com>
2019-08-06 10:20:36 +0200

    add template fro selector exercise

---
+++ selectors_exercise (2)/myscript.js
@@ -0,0 +1,38 @@
+/*
+ * My jQuery selectors
+ *
+ * Apply a style such as a yellow background to the following elements.
+ * Test your selectors one at a time.
+ */
+
+// all elements
+// $("*").css("background-color", "yellow");
+
+// all p elements
+
+
+// the p element with an ID of "intro"
+
+
+// all elements with a class of "note"
+
+
+// all DIV elements with a class of note
+
+
+// all p elements in the article element
+
+
+// the last element in any section which is a p tag
+
+
+// the first paragraph on the page
+
+
+// all p elements which have a link
+
+
+// the second li within nested ul element
+
+
+// every second and third p element
