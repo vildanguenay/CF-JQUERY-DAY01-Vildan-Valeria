50c347f9b8e5d248a8c3f1a18e1f870c98b6c064
Vildan Günay <vildan-guenay@hotmail.com>
2019-08-06 13:59:55 +0200

    intermediate

--- manipulating_html_page_exercise_files/myscript.js
+++ manipulating_html_page_exercise_files/myscript.js
@@ -1,15 +1,27 @@
+// $(document).ready(function() {
+// 	for (i = 0; i < 5; i++) {
+// 			var newDiv = $(`<div>DIV</div>`)
+// 			$("article").prepend(newDiv)
+// 	}
+// });
+
 $("form").submit(function(e) {
 
 	e.preventDefault();
 	// $("form").remove();	
 	
 	// CREATES 5 DIVS ON EACH CLICK
-	for (i = 0; i < 5; i++) {
-		var newDiv = $(`<div>DIV</div>`)
-		$("article").prepend(newDiv)
-	}
+	
 	// CREATES ONE DIV ON EACH CLICK
 	// var newDiv = $(`<div>DIV</div>`);
 	// $("article").prepend(newDiv);
+	
+	// Within the input field change the value to "Search for..."
+	// $("#msg").attr("value", "Search for...");
 
+	// Add a class name of "box" to each new DIVS
+	// $("div").addClass("box");
+
+	// Change the link to "www.codefactory.wien"
+	$("footer a").attr("href", "www.codefactory.wien");
 });
