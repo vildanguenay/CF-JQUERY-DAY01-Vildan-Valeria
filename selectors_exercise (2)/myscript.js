60f6dee18d90c02013094c45e8650ddc1528c3e3
Vildan Günay <vildan-guenay@hotmail.com>
2019-08-06 11:37:25 +0200

    basic

--- selectors_exercise (2)/myscript.js
+++ selectors_exercise (2)/myscript.js
@@ -9,30 +9,33 @@
 // $("*").css("background-color", "yellow");
 
 // all p elements
-
+// $("p").css("background-color", "yellow");
 
 // the p element with an ID of "intro"
-
+// $("#intro").css("background-color", "yellow");
 
 // all elements with a class of "note"
-
+// $(".note").css("background-color", "yellow");
+// $("p.note").css("background-color", "red");
 
 // all DIV elements with a class of note
-
+// $("div.note").css("background-color", "yellow");
 
 // all p elements in the article element
-
+// $("article p").css("background-color", "yellow");
 
 // the last element in any section which is a p tag
-
+// $("p:last-of-type").css("background-color", "yellow");
 
 // the first paragraph on the page
-
+// $("p:first").css("background-color", "yellow");
 
 // all p elements which have a link
-
+// $("p:has(a)").css("background-color", "yellow");
 
 // the second li within nested ul element
-
+// $("article ul li:nth-child(2)").css("background-color", "yellow");
 
 // every second and third p element
+$("p:nth-of-type(2)").css("background-color", "yellow");
+$("p:nth-of-type(3)").css("background-color", "yellow");
