fa5e9defd2fcaac668cd133f66383e973fcd068f
Valeria Nazarova <alira.vn@gmail.com>
2019-08-06 16:02:31 +0200

    add html structure

--- sample_webpage/myscript.js
+++ sample_webpage/myscript.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
 	// $("a").attr("href", "www.codefactory.wien");
+	
 	// $("code").css("color", "red");
+
 	// $("ol > li").css("background-color", "#2a7b90");
+
 	// $("#main form input").attr("value", "Express your opinion");
+
 	// $("img").attr("src", "https://codefactory.wien/wp-content/themes/hestia/cf-images/small-jpg/2-fullstack.jpg");
+
 	// $("h3:nth-of-type(3), blockquote").css({"background-color" : "orange", "font-style" : "italic"});
 	// $("#logo").css("color", "black");
 	// $("span.gray").css("color", "white");
+
 	// $(".date").remove();
+
 	// $("#sidebar > ul:first").append(`<li><a href="index.html">New Templates</a></li>`)
+
 	// $("#sidebar > ul:first").append(`<li><a href="#">Order Template</a></li>`);
+
 	// $("#sidebar > ul:first").append(`<li><a href="#">Contact Us</a></li>`);
+
 	var text = $("#sidebar > p:first").text(`"When someone says: 'I want a programming language in which I need only to say what I wish, and done', give him a lollipop."`);
 });
